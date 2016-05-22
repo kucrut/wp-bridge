de67c08313fbb06f0c41d23dd148ef6805f71758
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-22 17:11:20 +0800

    Test case for bridge_should_filter_result()

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -2,13 +2,20 @@
 
 class Bridge_Test_Plugin extends WP_UnitTestCase {
 
+	protected $client_id = 'minnie';
+
+
 	public function setUp() {
 		parent::setUp();
 
-		/** @var WP_REST_Server $wp_rest_server */
-		global $wp_rest_server;
-		$this->server = $wp_rest_server = new WP_Test_Spy_REST_Server;
-		do_action( 'rest_api_init' );
+		add_filter( 'bridge_client_ids', array( $this, '_register_client_id' ) );
+	}
+
+
+	public function _register_client_id( $client_ids ) {
+		$client_ids[] = $this->client_id;
+
+		return $client_ids;
 	}
 
 	/**
@@ -16,7 +23,7 @@ class Bridge_Test_Plugin extends WP_UnitTestCase {
 	 *
 	 * @covers ::bridge_load
 	 */
-	function test_bridge_load() {
+	public function test_bridge_load() {
 		$this->assertTrue( class_exists( 'WP_REST_Controller' ) );
 
 		$this->assertEquals( 11, has_action( 'init', '_bridge_add_extra_api_taxonomy_arguments' ) );
@@ -30,5 +37,18 @@ class Bridge_Test_Plugin extends WP_UnitTestCase {
 		$this->assertTrue( class_exists( 'Bridge_Rest_Term_Modifier' ) );
 
 		$this->assertEquals( 10, has_action( 'rest_api_init', 'bridge_register_routes' ) );
+	}
+
+
+	/**
+	 * @covers ::bridge_should_filter_result
+	 */
+	public function test_bridge_should_filter_result() {
+		$request = new WP_REST_Request( 'OPTIONS', '/wp/v2/posts' );
+
+		$this->assertFalse( bridge_should_filter_result( $request ) );
+
+		$request->set_header( 'X-Requested-With', $this->client_id );
+		$this->assertTrue( bridge_should_filter_result( $request ) );
 	}
 }
