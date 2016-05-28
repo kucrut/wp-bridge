8abfaf186bdd76feb89d9fe0bab889eb0ff5a275
Dzikri Aziz <kvcrvt@gmail.com>
2016-05-29 00:07:10 +0800

    Bump version: 0.2.0

--- bridge.php
+++ bridge.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Bridge
  * Description: Additional REST API endpoints and functionalities.
- * Version: 0.1.0
+ * Version: 0.2.0
  * Author: Dzikri Aziz
  * Author URI: http://kucrut.org
  * Plugin URI: https://github.com/kucrut/wp-bridge
